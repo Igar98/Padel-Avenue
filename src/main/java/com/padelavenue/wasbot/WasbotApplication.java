a44891656fced01ff888842ae4f4bc70f2b25b17
Anselmo <igarsan98@gmail.com>
2024-11-26 11:04:10 +0100

    first commit

---
+++ src/main/java/com/padelavenue/wasbot/WasbotApplication.java
@@ -0,0 +1,13 @@
+package com.padelavenue.wasbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/padelavenue/wasbot/WasbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padelavenue.wasbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
